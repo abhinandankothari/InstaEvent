2bbb1fd1eb40a17d9e8ffc66b5345b09a0b544c4
Abhinandan Kothari <abhinandankothari@gmail.com>
2015-12-25 11:24:25 +0530

    replaced asynctask with bolts, added user to post

--- app/src/main/java/instaevent/abhinandankothari/com/instaevent/models/Post.java
+++ app/src/main/java/instaevent/abhinandankothari/com/instaevent/models/Post.java
@@ -3,31 +3,29 @@ package instaevent.abhinandankothari.com.instaevent.models;
 import com.parse.ParseClassName;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
-
-/**
- * Created by vjdhama on 21/12/15.
- */
+import com.parse.ParseUser;
 
 @ParseClassName("Post")
-public class Post extends ParseObject{
+public class Post extends ParseObject {
     public static final String DESCRIPTION = "description";
     public static final String IMAGE = "image";
+    public static final String USER = "user";
 
-    public ParseFile getImage(){
+    @SuppressWarnings("unused - for parse framework")
+    public Post() {
+    }
+
+    public Post(ParseUser user, ParseFile image, String description) {
+        put(USER, user);
+        put(IMAGE, image);
+        put(DESCRIPTION, description);
+    }
+
+    public ParseFile getImage() {
         return getParseFile(IMAGE);
     }
 
-    public String getDescription(){
+    public String getDescription() {
         return getString(DESCRIPTION);
-    }
-
-    public void setImage(ParseFile image){
-        // TODO: Amir - 23/12/15 - use constants
-        put("image", image);
-    }
-
-    public void setDescription(String description){
-        // TODO: Amir - 23/12/15 - use constants
-        put("description", description);
     }
 }
--- app/src/main/java/instaevent/abhinandankothari/com/instaevent/views/ImageActivity.java
+++ app/src/main/java/instaevent/abhinandankothari/com/instaevent/views/ImageActivity.java
@@ -1,7 +1,6 @@
 package instaevent.abhinandankothari.com.instaevent.views;
 
 import android.app.ProgressDialog;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -9,12 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.parse.ParseFile;
+import com.parse.ParseUser;
 
 import java.io.File;
 
+import bolts.Continuation;
+import bolts.Task;
 import instaevent.abhinandankothari.com.instaevent.R;
 import instaevent.abhinandankothari.com.instaevent.models.Post;
 
@@ -44,29 +47,29 @@ public class ImageActivity extends AppCompatActivity {
             public void onClick(View v) {
                 final ProgressDialog progressDialog = ProgressDialog.show(ImageActivity.this, null, "Uploading", true, false);
                 String desc = description.getText().toString();
-                new AsyncTask<String, Void, String>() {
-
-                    @Override
-                    protected String doInBackground(String... params) {
-                        try {
-                            String url = params[0];
-                            Post post = new Post();
-                            post.setImage(new ParseFile(new File(url)));
-                            post.setDescription(params[1]);
-                            post.save();
-                        } catch (Exception e) {
-                            Log.e("APP_LOG", "Error while uploading image", e);
-                        }
-                        return "Success";
-                    }
-
-                    @Override
-                    protected void onPostExecute(String s) {
-                        super.onPostExecute(s);
-                        progressDialog.dismiss();
-                        finish();
-                    }
-                }.execute(imageUrl, desc);
+                Task.forResult(new Post(ParseUser.getCurrentUser(), new ParseFile(new File(imageUrl)), desc))
+                        .onSuccess(new Continuation<Post, Post>() {
+                            @Override
+                            public Post then(Task<Post> task) throws Exception {
+                                Post post = task.getResult();
+                                post.save();
+                                return post;
+                            }
+                        }, Task.BACKGROUND_EXECUTOR)
+                        .continueWith(new Continuation<Post, Void>() {
+                            @Override
+                            public Void then(Task<Post> task) throws Exception {
+                                progressDialog.dismiss();
+                                if (task.isFaulted()) {
+                                    Log.e("INSTA_LOG", "Post save failed", task.getError());
+                                    Toast.makeText(ImageActivity.this, "Post save failed", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(ImageActivity.this, "Post saved", Toast.LENGTH_SHORT).show();
+                                    finish();
+                                }
+                                return null;
+                            }
+                        }, Task.UI_THREAD_EXECUTOR);
             }
         });
     }
